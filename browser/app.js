544b82b6d89d5916ab3caa54ac341c07a16add2a
Nick Friedland <nick.friedland@gmail.com>
2019-02-04 21:34:16 -0800

    initial commit

---
+++ browser/app.js
@@ -0,0 +1,27 @@
+const $resultsArea = $('#results-area');
+const $urlInput = $('#article-url');
+
+// Use AJAX result to add readability rating results
+
+function showResults(res) {
+  // console.log('I AM JQUERY RES DATA HOPEFULLY', res);
+  let test = res;
+  let test1 = Object.values(test);
+  console.log('HELLO', test1);
+  if (test1.length > 0) {
+    for (let k in res) {
+      $resultsArea.append(`${k}: ${res[k]}<br>`);
+    }
+  }
+}
+
+$('form').on('submit', function(e) {
+  e.preventDefault();
+
+  $resultsArea.empty();
+
+  let url = $urlInput.val();
+  $urlInput.val('');
+
+  $.post('http://localhost:3000/', { url }, showResults);
+});
