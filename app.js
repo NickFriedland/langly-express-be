65e5a4bdeaf0edb9c11eb27caf4f71361066de71
Nick Friedland <nick.friedland@gmail.com>
2019-03-07 13:17:12 -0800

    Getting rid of Mercury related package

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const axios = require('axios');
-const htmlToText = require('html-to-text');
 
 // enable cors on all routes
 app.use(cors());
