16e2d19a54425c5a1996bced9937d8ce2f6d9265
Nick Friedland <nick.friedland@gmail.com>
2019-03-09 14:57:16 -0800

    Getting rid of Mercury API key

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 // Shared config for application
 const path = require('path');
 
-process.env.MercuryAPIKey = 'HTNOPBUZzY67pfYVkCrAZtDlXlOuNhv4ZK4BOaYz';
 process.env.TranslateProjectId = 'hackathon-langly-1546470577089';
 process.env.GOOGLE_APPLICATION_CREDENTIALS = path.join(
   __dirname,
